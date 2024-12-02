1d782f1bde2cc57747d516dbf6669aa9c8795780
Huachun <13895824+hanezu@users.noreply.github.com>
2024-12-02 16:30:04 +0000

    update name

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -11,7 +11,7 @@ public class QueryProcessor {
         }
 
         if (query.contains("your name")) {
-            return "RobTest";
+            return "QA_survival";
         }
 
         return "";
